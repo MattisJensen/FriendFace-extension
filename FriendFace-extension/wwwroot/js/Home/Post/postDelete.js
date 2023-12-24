b9c645d1bafc16dcaa83c64731829253cb20f5ac
MattisJensen <115666021+MattisJensen@users.noreply.github.com>
2023-12-24 16:09:35 +0100

    Ajax for post deletion

--- FriendFace-extension/wwwroot/js/Home/Post/postDelete.js
+++ FriendFace-extension/wwwroot/js/Home/Post/postDelete.js
@@ -22,27 +22,47 @@ function postDelete(deleteField, postId) {
     deleteField.css('color', 'rgb(255,255,255)');
     deleteField.css('backgroundColor', 'rgb(220,53,69)');
 
-
     // Add listener to execute delete request
-    deleteField.on('click', deleteRequest);
+    deleteField.on('click', function (e) {
+        deleteRequest(postId)
+    });
 
     // Add listener to reset the delete text when the dropdown is closed
     $(document).on('click', function (e) {
         if (!$(e.target).closest('.dropdown-menu').length) {
-            resetDeleteText(postId, deleteField);
+            resetDeleteText(postId);
         }
     });
-
-    function deleteRequest() {
-        window.location.href = '/Home/DeletePost?postId=' + postId;
-    }
-
-    function resetDeleteText(postId, deleteField) {
-        deleteField.text('Delete');
-        // reset color to default
-        deleteField.css('color', '');
-        deleteField.css('backgroundColor', '');
-
-        deleteField.off('click', deleteRequest);
-    }
 }
+
+function deleteRequest(postId) {
+    var post = $('#postContainer-' + postId);
+    post.hide();
+
+    $.ajax({
+        type: 'PUT',
+        url: '/Home/DeletePost',
+        contentType: 'application/json',
+        data: JSON.stringify(postId),
+        dataType: 'json',
+        success: function (success) {
+            post.remove();
+        },
+        error: function (error) {
+            post.show();
+            resetDeleteText(postId);
+        }
+    });
+}
+
+function resetDeleteText(postId) {
+    var deleteField = $('#deleteField-' + postId)
+    
+    deleteField.text('Delete');
+    // reset color to default
+    deleteField.css('color', '');
+    deleteField.css('backgroundColor', '');
+
+    deleteField.off('click', deleteRequest);
+}
+
