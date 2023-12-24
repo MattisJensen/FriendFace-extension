ed642baaef8ad44acb5c08c623132558496ba4e0
MattisJensen <115666021+MattisJensen@users.noreply.github.com>
2023-12-24 16:08:33 +0100

    Ajax for liking

--- FriendFace-extension/wwwroot/js/Home/Post/postToggleLike.js
+++ FriendFace-extension/wwwroot/js/Home/Post/postToggleLike.js
@@ -14,5 +14,36 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function toggleLike(postId) {
-    window.location.href = '/Home/ToggleLikePost?postId=' + postId;
+    toggleLikeIcon(postId);
+    updateLikeCount(postId);
+    
+    $.ajax({
+        type: 'POST',
+        url: '/Home/ToggleLikePost',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(postId),
+        success: function (success) {
+        },
+        error: function (error) {
+            toggleLikeIcon(postId);
+            updateLikeCount(postId);
+        }
+    });
 }
+
+function toggleLikeIcon(postId) {
+    var likeButton = $('#likeButton-' + postId);
+    likeButton.toggleClass('far fas');
+    likeButton.toggleClass('color-inherit color-red');
+}
+
+function updateLikeCount(postId) {
+    // increase or decrease like count
+    var likeButton = $('#likeButton-' + postId);
+    var likeCount = $('#likeCount-' + postId);
+    var likeCountNumber = parseInt(likeCount.text());
+    likeCountNumber = likeButton.hasClass('fas') ? likeCountNumber + 1 : likeCountNumber - 1;
+    likeCount.text(likeCountNumber);
+}
+
